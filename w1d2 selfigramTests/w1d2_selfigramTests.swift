50a98eaf683041b9d496511503378f7a3056b335
Swetapanchal92 <30636636+Swetapanchal92@users.noreply.github.com>
2017-08-10 19:20:14 -0400

    Initial Commit

---
+++ w1d2 selfigramTests/w1d2_selfigramTests.swift
@@ -0,0 +1,36 @@
+//
+//  w1d2_selfigramTests.swift
+//  w1d2 selfigramTests
+//
+//  Created by Nitin Panchal on 2017-08-10.
+//  Copyright © 2017 Sweta panchal. All rights reserved.
+//
+
+import XCTest
+@testable import w1d2_selfigram
+
+class w1d2_selfigramTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
